54e8a3ae6fd0877a2e5a53d7bb2cd038ecfafb64
John Kelley <john@kelley.ca>
2020-07-27 18:36:27 -0400

    TARGET_PSP version of get_time()

--- src/pc/gfx/gfx_pc.c
+++ src/pc/gfx/gfx_pc.c
@@ -153,12 +153,23 @@ static size_t buf_vbo_num_tris;
 static struct GfxWindowManagerAPI *gfx_wapi;
 static struct GfxRenderingAPI *gfx_rapi;
 
+#if defined(TARGET_PSP)
+#include <sys/time.h>
+static unsigned long get_time(void) {
+	struct timeval tp;
+
+	gettimeofday(&tp, NULL);
+
+	return (unsigned long)(tp.tv_sec * 1000000) + tp.tv_usec;
+}
+#else
 #include <time.h>
 static unsigned long get_time(void) {
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
     return (unsigned long)ts.tv_sec * 1000000 + ts.tv_nsec / 1000;
 }
+#endif
 
 static void gfx_flush(void) {
     if (buf_vbo_len > 0) {
