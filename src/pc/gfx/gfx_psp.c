d6e98c4f613baaa16c723391e091021811c46564
HaydenKow <819028+mrneo240@users.noreply.github.com>
2020-08-01 15:24:58 -0400

    feat: redo psp egl init
    - actually run the game
    - swap buffers

--- src/pc/gfx/gfx_psp.c
+++ src/pc/gfx/gfx_psp.c
@@ -1,20 +1,32 @@
 #if defined(TARGET_PSP)
 #include <pspsdk.h>
 #include <pspdisplay.h>
+#include <pspkernel.h>
 #include <stdio.h>
+#include <GL/glut.h>
 #include <GLES/egl.h>
 #include <GLES/gl.h>
 #include "gfx_window_manager_api.h"
 #include "gfx_screen_config.h"
 
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+#define INFO_MSG(x) printf("%s %s\n", __FILE__ ":" TOSTRING(__LINE__), x)
+
 int exitCallback(int arg1, int arg2, void *common)
 {
+	(void)arg1;
+	(void)arg2;
+	(void)common;
 	//exitRequest = 1;
+	sceKernelExitGame();
 	return 0;
 }
 
 int callbackThread(SceSize args, void *argp)
 {
+	(void)args;
+	(void)argp;
 	int cbid;
 
 	cbid = sceKernelCreateCallback("Exit Callback", exitCallback, NULL);
@@ -41,39 +53,69 @@ int setupCallbacks(void)
 static EGLDisplay dpy;
 static EGLContext ctx;
 static EGLSurface surface;
-static const int screen_attrib[] = {
-	EGL_RED_SIZE,		8,
-	EGL_GREEN_SIZE,		8,
-	EGL_BLUE_SIZE,		8,
-	EGL_ALPHA_SIZE,		0,
-	EGL_STENCIL_SIZE,	0,
-	EGL_DEPTH_SIZE,		0,
-	EGL_NONE
-};
+static EGLint width = 480;
+static EGLint height = 272;
+static unsigned int glut_display_mode = 0;
 
-static const int pbuffer_attrib[] = {
-	EGL_RED_SIZE,	8,
-	EGL_GREEN_SIZE,	8,
-	EGL_BLUE_SIZE,	8,
-	EGL_ALPHA_SIZE,	8,
-	EGL_DEPTH_SIZE,	16,
-
-	EGL_NONE
-};
-
-static EGLConfig screen_config, pbuffer_config;
-static EGLSurface screen;
+static EGLint attrib_list[] = {
+    EGL_RED_SIZE, 8,     /* 0 */
+    EGL_GREEN_SIZE, 8,   /* 2 */
+    EGL_BLUE_SIZE, 8,    /* 4 */
+    EGL_ALPHA_SIZE, 0,   /* 6 */
+    EGL_STENCIL_SIZE, 0, /* 8 */
+    EGL_DEPTH_SIZE, 0,   /* 10 */
+    EGL_NONE};
 
 void gfx_psp_init(const char *game_name, bool start_in_fullscreen) {
     fprintf(stderr, "%s called for %s, should %sstart in fullscreen\n", __FUNCTION__, game_name, start_in_fullscreen ? "" : "not ");
     fprintf(stderr, "setupCallbacks() returned %d\n", setupCallbacks());
     pspDebugScreenSetBackColor(0xFF0000FF);
     pspDebugScreenInitEx(NULL, PSP_DISPLAY_PIXEL_FORMAT_8888, 1);
-    dpy = eglGetDisplay(0);
-    eglInitialize(dpy, NULL, NULL);
-    ctx = eglCreateContext(dpy, screen_config, EGL_NO_CONTEXT, NULL);
-    screen = eglCreateWindowSurface(dpy, screen_config, 0, NULL);
-    eglMakeCurrent(dpy, screen, screen, ctx);
+
+  EGLConfig config;
+  EGLint num_configs;
+
+  /* pass NativeDisplay=0, we only have one screen... */
+  dpy = eglGetDisplay(0);
+  eglInitialize(dpy, NULL, NULL);
+
+#if 0
+	psp_log("EGL vendor \"%s\"\n", eglQueryString(dpy, EGL_VENDOR));
+	psp_log("EGL version \"%s\"\n", eglQueryString(dpy, EGL_VERSION));
+	psp_log("EGL extensions \"%s\"\n", eglQueryString(dpy, EGL_EXTENSIONS));
+#endif
+
+  /* Select type of Display mode:   
+     Double buffer 
+     RGBA color
+     Alpha components supported 
+     Depth buffered for automatic clipping */
+  glut_display_mode = (GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);
+
+  if (glut_display_mode & GLUT_ALPHA)
+    attrib_list[7] = 8;
+  if (glut_display_mode & GLUT_STENCIL)
+    attrib_list[9] = 8;
+  if (glut_display_mode & GLUT_DEPTH)
+    attrib_list[11] = 16;
+
+  eglChooseConfig(dpy, attrib_list, &config, 1, &num_configs);
+
+  if (num_configs == 0) {
+    fprintf(stderr, "glutCreateWindow: eglChooseConfig returned no configurations for display mode %x\n",
+                glut_display_mode);
+  }
+
+#if 0
+	fprintf(stderr, ("eglChooseConfig() returned config 0x%04x\n", (unsigned int)config);
+#endif
+
+  eglGetConfigAttrib(dpy, config, EGL_WIDTH, &width);
+  eglGetConfigAttrib(dpy, config, EGL_HEIGHT, &height);
+
+  ctx = eglCreateContext(dpy, config, NULL, NULL);
+  surface = eglCreateWindowSurface(dpy, config, 0, NULL);
+  eglMakeCurrent(dpy, surface, surface, ctx);
 }
 
 void gfx_psp_set_keyboard_callbacks(bool (*on_key_down)(int scancode), bool (*on_key_up)(int scancode), void (*on_all_keys_up)(void)) {
@@ -92,7 +134,9 @@ void gfx_psp_set_fullscreen(bool enable) {
 }
 
 void gfx_psp_main_loop(void (*run_one_game_iter)(void)) {
-    (void)run_one_game_iter;
+	while (1) {
+		run_one_game_iter();
+	}
 }
 
 void gfx_psp_get_dimensions(uint32_t *width, uint32_t *height) {
@@ -115,6 +159,7 @@ void gfx_psp_swap_buffers_begin(void) {
 
 void gfx_psp_swap_buffers_end(void) {
 	fprintf(stderr, "%s called\n", __FUNCTION__);
+	eglSwapBuffers(dpy, surface);
 }
 
 double gfx_psp_get_time(void) {
