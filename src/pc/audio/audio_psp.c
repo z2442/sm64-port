465ce8c4b52073e62d7dae612f16915c0ef73600
HaydenKow <819028+mrneo240@users.noreply.github.com>
2020-08-29 23:50:50 -0400

    fix: remove unneeded line

--- src/pc/audio/audio_psp.c
+++ src/pc/audio/audio_psp.c
@@ -15,6 +15,9 @@
 static int chan = -1;
 static int samples = 0;
 
+extern void memcpy_vfpu( void* dst, const void* src, size_t size );
+extern void memcpy4(void *dest, const void *src, size_t count);
+
 /* Double Buffer */
 static int cur_snd_buf = 0;
 uint16_t snd_buffer_internal[PSP_AUDIO_SAMPLES_DESIRED * PSP_AUDIO_GIVEN_BUFFERS * PSP_AUDIO_CHANNELS * 2] /* At worst, 4 whole frames */
@@ -57,7 +60,7 @@ static void audio_psp_play(const uint8_t *buf, size_t len) {
     int new_samples = len / (sizeof(short) * PSP_AUDIO_CHANNELS);
 
     sceKernelDcacheWritebackInvalidateRange(buf, len);
-    memcpy(snd_buffer[cur_snd_buf], buf, len);
+    memcpy_vfpu(snd_buffer[cur_snd_buf], buf, len);
     sceKernelDcacheWritebackInvalidateRange(snd_buffer[cur_snd_buf], len);
 
     /* Check if we need to reacquire channel or num samples */
@@ -73,7 +76,7 @@ static void audio_psp_play(const uint8_t *buf, size_t len) {
         sceAudioOutput2ChangeLength(new_samples);
         samples = new_samples;
     }
-    sceKernelDcacheWritebackRange(snd_buffer[cur_snd_buf], len);
+
     sceAudioOutput2OutputBlocking(PSP_AUDIO_VOLUME_MAX, snd_buffer[cur_snd_buf]);
 
     /* Swap double buffer back */
